0410521cb4d7394fff89479b196a32f4beaa1d8e
rodrigodata <warodrigoandrade@gmail.com>
2015-12-29 16:20:57 -0200

    change js file

--- hexClock.js
+++ hexClock.js
@@ -2,27 +2,21 @@
 if (Meteor.isClient) {
 
        Template.horas.helpers({
-              
+
        'hora': function tempo(){
-         
+
          d = new Date();
          var horasVar = d.getHours();
          var minutosVar = d.getMinutes();
          var segundosVar = d.getSeconds();
          var relogioFinal = horasVar +" : "+ minutosVar +" : "+ segundosVar;
-
-         if (minutosVar < 10){minutosVar = 0 + minutosVar};
-         if (segundosVar < 10){segundosVar = 0 + segundosVar};
-         
-
-         //var hex = "#" + horasVar + minutosVar + segundosVar;
-         //document.body.style.background = hex;
+     
 
          console.log(horasVar, minutosVar, segundosVar);
          //setTimeout(function(){ tempo();}, 1000);
          return document.write(relogioFinal);
-        
-        
+
+
     }
 
 
@@ -33,6 +27,6 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-     
-            
+
+
 }
